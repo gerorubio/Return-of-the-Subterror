a242c523fc5ed7e3db617ce00d38642ba3959c24
gerorubio <vulcandro@hotmail.com>
2025-07-31 16:00:09 -0600

    Subterror Nemesis Sorcerer Flip effect finish v0.1

--- script/c410123450.lua
+++ script/c410123450.lua
@@ -22,7 +22,7 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.FaceUpSubterrorTarget)
-	e2:SetOperation(s.setop)
+	e2:SetOperation(s.SpecialSummonSetOp)
 	c:RegisterEffect(e2)
 end
 
@@ -38,11 +38,9 @@ function s.FaceUpSubterrorMonster(c)
 	return c:IsSetCard(SET_SUBTERROR) and c:IsFaceup() and c:IsCanTurnSet()
 end
 
-function s.spfilter(c)
-	return c:IsSetCard(SET_SUBTERROR) and not c:IsCode(id) and c:IsMonsterCard()
+function s.SubterrorMonsterInDeck(c, e, tp)
+	return c:IsSetCard(SET_SUBTERROR) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e, 0, tp, false, true)
 end
-
-
 --
 function s.GyTargetToHand(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.GyTarget,tp,LOCATION_GRAVE,0,1,nil) end
@@ -60,25 +58,27 @@ end
 
 function s.FaceUpSubterrorTarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.FaceUpSubterrorMonster(chkc) end
-	if chk ==  0 then return Duel.GetLocationCount(tp, LOCATION_MZONE > 0) and Duel.IsExistingMatchingCard(s.FaceUpSubterrorMonster, tp, LOCATION_MZONE, 0, 1, nill)
+	if chk ==  0 then
+		return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 -- There is space to special summon
+			and Duel.IsExistingMatchingCard(s.FaceUpSubterrorMonster, tp, LOCATION_MZONE, 0, 1, nill) -- There is Subtteror monsteer in deck to special summon
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectTarget(tp,s.FaceUpSubterrorMonster, tp, LOCATION_MZONE, LOCATION_MZONE, 1, 1, nil)
+	Duel.SetOperationInfo(0, CATEGORY_POSITION, g, 1, 0, 0)
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_DECK)
 end
 
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+function s.SpecialSummonSetOp(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
-	local g=Group.FromCards(c,tc)
-	if g:IsExists(function(c,e) return not c:IsFaceup() or not c:IsRelateToEffect(e) end,1,nil,e) then return end
-	Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		if Duel.ChangePosition(tc, POS_FACEDOWN_DEFENSE) ~= 0 then
+			if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+			local g=Duel.SelectMatchingCard(tp, s.SubterrorMonsterInDeck, tp, LOCATION_DECK, 0, 1, 1, nil, e, tp)
+			if #g>0 then
+				Duel.SpecialSummon(g,0,tp,tp,false,true,POS_FACEDOWN_DEFENSE)
+				Duel.ConfirmCards(1-tp,g)
+			end
+		end
+	end
 end
-
--- function s.setcon1(e,tp,eg,ep,ev,re,r,rp)
--- 	-- Checks if COUNT cards exist in locations S on PLAYER side of the field and O on their opponent's for which F returns true, except (Card ex)
--- 	return not Duel.IsExistingMatchingCard(
--- 		aux.FaceupFilter(Card.IsSetCard,SET_SUBTERROR), -- F
--- 		tp, -- PLAYER
--- 		LOCATION_ONFIELD, -- S
--- 		0, -- O
--- 		1, --  COUNT
--- 		nill -- e:GetHandler()
--- 	)
--- end
