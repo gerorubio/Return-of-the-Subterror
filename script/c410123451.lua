8ace66e47080823f325ac23ef5e7a93c116b6af8
gerorubio <vulcandro@hotmail.com>
2025-08-02 21:08:57 -0600

    Subterror Disruption v0.2

--- script/c410123451.lua
+++ script/c410123451.lua
@@ -17,12 +17,40 @@ function s.initial_effect(c)
 	e2:SetCategory(CATEGORY_POSITION)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_SUMMON_SUCCESS + EVENT_SPSUMMON_SUCCESS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_STZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.FaceDownSubterrorTarget)
 	e2:SetOperation(s.ChangeToFaceUPSubterrorOp)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCountLimit(1,{id,1}) -- Check
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCountLimit(1,{id,1}) -- Check
+	e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e4)
+	-- When a monster is summoned flip face down a subterror monster
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_POSITION)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e5:SetRange(LOCATION_STZONE)
+	e5:SetCountLimit(1,{id,1})
+	e5:SetTarget(s.FaceUpSubterrorTarget)
+	e5:SetOperation(s.ChangeToDownUPSubterrorOp)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCountLimit(1,{id,1}) -- Check
+	e6:SetCode(EVENT_SUMMON_SUCCESS)
+	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCountLimit(1,{id,1}) -- Check
+	e7:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e7)
 end
 
 s.listed_series={SET_SUBTERROR}
@@ -30,6 +58,14 @@ s.listed_names={id}
 
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(SET_SUBTERROR) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.FaceDownSubterrorMonster(c)
+	return c:IsSetCard(SET_SUBTERROR) and c:IsFacedown() and c:IsCanChangePosition()
+end
+
+function s.FaceUpSubterrorMonster(c)
+	return c:IsSetCard(SET_SUBTERROR) and c:IsFaceup() and c:IsCanChangePosition()
 end
 
 function s.SpecialSummonFromDeckFaceDown(e,tp,eg,ep,ev,re,r,rp)
@@ -45,21 +81,37 @@ end
 
 -- Effect 2
 function s.FaceDownSubterrorTarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFacedown() and s.FaceDownSubterrorFilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFacedown,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFacedown() end
+	if chk==0 then return Duel.IsExistingTarget(s.FaceDownSubterrorMonster, tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g=Duel.SelectTarget(tp,Card.IsFacedown,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp, s.FaceDownSubterrorMonster, tp, LOCATION_MZONE, LOCATION_MZONE, 1, 1, nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
 
 function s.ChangeToFaceUPSubterrorOp(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)>0 then
-		if tc:IsFacedown() and tc:IsCanChangePosition() then
-			local pos=Duel.SelectPosition(tp, tc, POS_FACEUP_ATTACK+POS_FACEUP_DEFENSE)
-			Duel.ChangePosition(tc, pos)
-		end
+	if tc:IsFacedown() and tc:IsCanChangePosition() then
+		local pos=Duel.SelectPosition(tp, tc, POS_FACEUP_ATTACK+POS_FACEUP_DEFENSE)
+		Duel.ChangePosition(tc, pos)
+	end
+end
+
+function s.FaceUpSubterrorTarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(s.FaceUpSubterrorMonster, tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp, s.FaceUpSubterrorMonster, tp, LOCATION_MZONE, LOCATION_MZONE, 1, 1, nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+
+function s.ChangeToDownUPSubterrorOp(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsCanChangePosition() then
+		local pos=Duel.SelectPosition(tp, tc, POS_FACEDOWN_DEFENSE)
+		Duel.ChangePosition(tc, pos)
 	end
 end
